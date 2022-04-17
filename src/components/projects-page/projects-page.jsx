5fb8fb6338e92b2f244fd98b4fc3e8208323a72e
Harry McGoldrick <harry_mcgoldrick@msn.com>
2022-04-17 14:29:55 +0100

    add scroll animations to project cards

--- src/components/projects-page/projects-page.jsx
+++ src/components/projects-page/projects-page.jsx
@@ -3,8 +3,19 @@ import { DescriptorCard } from '../descriptor-card/descriptor-card';
 import { ProjectCard } from '../project-card/project-card';
 import './projects-page.css';
 import projects from './project-list';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
 
 export const ProjectsPage = () => {
+
+    useEffect(() => {
+        Aos.init({
+            offset: 300,
+            delay: 50
+        });
+    });
 
     return (
         <>
@@ -20,18 +31,21 @@ export const ProjectsPage = () => {
                     {projects.map((element, index) => {
                         if (index % 2 === 0) {
                             return(
+                            <div data-aos="fade-up">
                                 <Row id={index} style={{paddingBottom: '8rem'}}>
-                                <Col xs={6} className="text-holder" style={{paddingRight: '5rem'}}>
-                                    <DescriptorCard project={element}
-                                    ></DescriptorCard>
-                                </Col>
-                                <Col xs={6} className="image-holder" style={{paddingLeft: '1rem'}}>
-                                    <ProjectCard img={element.img} title='test' description='test'> </ProjectCard>
-                                </Col>
-                            </Row>
+                                    <Col xs={6} className="text-holder" style={{paddingRight: '5rem'}}>
+                                        <DescriptorCard project={element}
+                                        ></DescriptorCard>
+                                    </Col>
+                                    <Col xs={6} className="image-holder" style={{paddingLeft: '1rem'}}>
+                                        <ProjectCard img={element.img} title='test' description='test'> </ProjectCard>
+                                    </Col>
+                                </Row>
+                            </div>
                             )
                         } else {
                             return (
+                            <div data-aos="fade-up">
                                 <Row id={index} style={{paddingBottom: '8rem'}}>
                                     <Col xs={6} className="image-holder">
                                         <ProjectCard img={element.img} title='test' description='test'> </ProjectCard>
@@ -41,6 +55,7 @@ export const ProjectsPage = () => {
                                         ></DescriptorCard>
                                     </Col>
                                 </Row>
+                            </div>
                                 )
                         }
                     })}
